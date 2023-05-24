fdc9695b1ac0da7d7997ddac60a8bc506c242b97
Ranajit Roy <royranajit1@gmail.com>
2023-05-24 14:29:02 -0600

    maven commit

---
+++ services/participator-service/src/test/java/com/rmauction/participatorservice/ParticipatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmauction.participatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
