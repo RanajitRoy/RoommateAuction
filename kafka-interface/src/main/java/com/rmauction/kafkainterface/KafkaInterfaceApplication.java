430443baed6505f95e3a1647e2b46da1716ce2e1
Ranajit Roy <royranajit1@gmail.com>
2023-05-25 17:52:45 -0600

    kafka initial project file commit

---
+++ kafka-interface/src/main/java/com/rmauction/kafkainterface/KafkaInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.rmauction.kafkainterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaInterfaceApplication.class, args);
+	}
+
+}
---
+++ kafka-interface/src/test/java/com/rmauction/kafkainterface/KafkaInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmauction.kafkainterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
